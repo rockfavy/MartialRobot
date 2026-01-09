56bda363f0ee05db69ff5609772a08fb277fc3f0
Rock Favy <rock.dev@favysoft.net>
2026-01-09 09:25:13 +0200

    Instruction parser added

---
+++ MartialRobot.Tests/Features/BasicRobotMovement/Parsers/InstructionParser.cs
@@ -0,0 +1,41 @@
+﻿using MartialRobot.Features.BasicRobotMovement.Parsers;
+
+namespace MartialRobot.Tests.Features.BasicRobotMovement.Parsers
+{
+    public class InstructionParserTests
+    {
+        [Fact]
+        public void Parse_ValidInstructions_ReturnsString()
+        {
+            var parser = new InstructionParser();
+            var result = parser.Parse("RFRFRFRF");
+
+            Assert.Equal("RFRFRFRF", result);
+        }
+
+        [Fact]
+        public void Parse_ValidInstructions_AllCommands_ReturnsString()
+        {
+            var parser = new InstructionParser();
+            var result = parser.Parse("LRF");
+
+            Assert.Equal("LRF", result);
+        }
+
+        [Fact]
+        public void Parse_InvalidCharacter_X_ThrowsException()
+        {
+            var parser = new InstructionParser();
+
+            Assert.Throws<ArgumentException>(() => parser.Parse("RFX"));
+        }
+
+        [Fact]
+        public void Parse_EmptyString_ThrowsException()
+        {
+            var parser = new InstructionParser();
+
+            Assert.Throws<ArgumentException>(() => parser.Parse(""));
+        }
+    }
+}
---
+++ MartialRobot/Features/BasicRobotMovement/Parsers/InstructionParser.cs
@@ -0,0 +1,24 @@
+﻿namespace MartialRobot.Features.BasicRobotMovement.Parsers
+{
+    public class InstructionParser
+    {
+        public string Parse(string instructionLine)
+        {
+            if (string.IsNullOrWhiteSpace(instructionLine))
+            {
+                throw new ArgumentException("Instruction line cannot be empty");
+            }
+
+            foreach (var ch in instructionLine)
+            {
+                var upperCh = char.ToUpper(ch);
+                if (upperCh != 'L' && upperCh != 'R' && upperCh != 'F')
+                {
+                    throw new ArgumentException($"Invalid instruction character: {ch}");
+                }
+            }
+
+            return instructionLine;
+        }
+    }
+}
