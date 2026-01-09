b1bb26c9ef7f36f2eaeca77cb91d52a672b66f32
Rock Favy <rock.dev@favysoft.net>
2026-01-09 09:50:59 +0200

    scent tracking functionality added

---
+++ MartialRobot.Tests/Features/ScentTracking/ScentTrackerTests.cs
@@ -0,0 +1,69 @@
+﻿using MartialRobot.Features.BasicRobotMovement.Models;
+using MartialRobot.Features.ScentTracking;
+
+namespace MartialRobot.Tests.Features.ScentTracking
+{
+    public class ScentTrackerTests
+    {
+        [Fact]
+        public void HasScent_AfterAddingScent_ReturnsTrue()
+        {
+            var tracker = new ScentTracker();
+            var point = new CoordonatePoint(5, 3);
+
+            tracker.AddScent(point);
+
+            Assert.True(tracker.HasScent(point));
+        }
+
+        [Fact]
+        public void HasScent_WithoutAddingScent_ReturnsFalse()
+        {
+            var tracker = new ScentTracker();
+            var point = new CoordonatePoint(5, 3);
+
+            Assert.False(tracker.HasScent(point));
+        }
+
+        [Fact]
+        public void HasScent_DifferentPoint_ReturnsFalse()
+        {
+            var tracker = new ScentTracker();
+            var point1 = new CoordonatePoint(5, 3);
+            var point2 = new CoordonatePoint(5, 4);
+
+            tracker.AddScent(point1);
+
+            Assert.False(tracker.HasScent(point2));
+        }
+
+        [Fact]
+        public void HasScent_SameCoordinates_DifferentInstance_ReturnsTrue()
+        {
+            var tracker = new ScentTracker();
+            var point1 = new CoordonatePoint(5, 3);
+            var point2 = new CoordonatePoint(5, 3);
+
+            tracker.AddScent(point1);
+
+            Assert.True(tracker.HasScent(point2));
+        }
+
+        [Fact]
+        public void AddScent_MultiplePoints_TracksAll()
+        {
+            var tracker = new ScentTracker();
+            var point1 = new CoordonatePoint(5, 3);
+            var point2 = new CoordonatePoint(3, 2);
+            var point3 = new CoordonatePoint(0, 0);
+
+            tracker.AddScent(point1);
+            tracker.AddScent(point2);
+            tracker.AddScent(point3);
+
+            Assert.True(tracker.HasScent(point1));
+            Assert.True(tracker.HasScent(point2));
+            Assert.True(tracker.HasScent(point3));
+        }
+    }
+}
---
+++ MartialRobot/Features/ScentTracking/ScentTracker.cs
@@ -0,0 +1,19 @@
+﻿using MartialRobot.Features.BasicRobotMovement.Models;
+
+namespace MartialRobot.Features.ScentTracking
+{
+    public class ScentTracker
+    {
+        private readonly HashSet<CoordonatePoint> _scents = new();
+
+        public void AddScent(CoordonatePoint point)
+        {
+            _scents.Add(point);
+        }
+
+        public bool HasScent(CoordonatePoint point)
+        {
+            return _scents.Contains(point);
+        }
+    }
+}
